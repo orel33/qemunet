1a1d35463cfb4c8293e9867833cdb3318c82b9c5
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-03-28 14:13:42 +0100

    update docker fles

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker build -t "orel33/qemunet:latest" . && docker push "orel33/qemunet:latest"
+docker build -t "orel33/qemu:latest" -f Dockerfile.qemu . && docker push "orel33/qemu:latest"
+docker build -t "orel33/qemunet:latest" -f Dockerfile.qemunet . && docker push "orel33/qemunet:latest"
