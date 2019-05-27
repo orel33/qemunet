1260a9f5e0572d9232895b9fe5c8d51410253fb0
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-05-27 12:01:49 +0200

    add hax accelerator if available

--- qemunet.sh
+++ qemunet.sh
@@ -617,7 +617,7 @@ HOST() {
     
     # accelerator option (by default)
     # if [ "$NOACCEL" -eq 0 ] ; then CMD="$CMD -enable-kvm" ; fi
-    if [ "$NOACCEL" -eq 0 ] ; then CMD="$CMD -M accel=kvm:hvf" ; fi
+    if [ "$NOACCEL" -eq 0 ] ; then CMD="$CMD -M accel=kvm:hax:hvf" ; fi
     
     # specific QEMU options
     CMD="$CMD $HOSTOPT"
