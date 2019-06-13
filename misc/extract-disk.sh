9fe1507746dd8b4da229841b9a4e65ab5a865613
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-06-13 12:16:56 +0200

    update xchain demo

---
+++ misc/extract-disk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+[ $# -gt 2 ] && echo "$0 <disk.img> [<path/to/dir>]" && exit 0
+
+DISK=$1
+DISKDIR=$2
+[ -z "$DISKDIR" ] && DISKDIR=$(mktemp -d)
+
+echo "disk=$DISK"
+echo "target disk dir=$DISKDIR"
+
+# [ ! -d $DISKDIR ] && echo "Error: target disk directory not found!" && exit 0
+[ ! -d $DISKDIR ] && mkdir -p $DISKDIR
+[ ! -f $DISK ] && echo "Error: disk file not found!" && exit 0
+
+sudo mount -o loop,uid=$UID,gid=$UID $DISK $DISKDIR
+# sudo mount -o loop $DISK $DISKDIR
+
+# sudo cp -v -rf $INPUTDIR/* $MNTDIR
+
+echo "Warning! Don't forget to umount $DISKDIR..."
+
+# eof
---
+++ misc/generate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+[ $# -ne 2 ] && echo "$0 <path/to/dir/> <archive.tgz>" && exit 0
+
+SCRIPTDIR=$(dirname $(realpath $0))
+SRCDIR=$1
+ARCHIVE=$2
+TMPDIR=$(mktemp -d)
+
+echo "SRCDIR: $SRCDIR"
+echo "ARCHIVE: $ARCHIVE"
+echo "TMPDIR: $TMPDIR"
+
+[ ! -d $SRCDIR ] && echo "Error: source directory not found!" && exit 0
+[ -f $ARCHIVE ] && echo "Error: archive file already exists!" && exit 0
+
+cp -rf $SRCDIR/* $TMPDIR/
+
+for HOST in $TMPDIR/* ; do
+    if [ -d $HOST ] ; then
+	echo "HOST: $HOST"
+	$SCRIPTDIR/create-disk.sh $HOST $HOST.disk
+	rm -rf $HOST
+    fi
+done
+
+( cd $TMPDIR && tar cvzSf $ARCHIVE * )
+mv $TMPDIR/$ARCHIVE .
+echo "Done!"
