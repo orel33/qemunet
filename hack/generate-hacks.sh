76d3450a2aa322bd48716171315a6b0544e7332a
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2020-04-16 17:41:42 +0200

    encrypted disk

--- hack/generate-hacks.sh
+++ hack/generate-hacks.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for DIR in $(find . -mindepth 1 -maxdepth 1 -type d) ; do    
+ALL=$(find . -mindepth 1 -maxdepth 1 -type d)
+
+for DIR in $ALL ; do    
     HACK="$DIR.tgz"
     rm -f $HACK
     echo "########## Generate $HACK ##########" 
--- misc/create-disk-encrypted.sh
+++ misc/create-disk-encrypted.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
-[ $# -ne 2 ] && echo "$0 <path/to/input/dir> <disk.img>" && exit
+[ $# -ne 3 ] && echo "$0 <path/to/input/dir> <disk.img> <password>" && exit
 
 INPUTDIR=$1
 DISK=$2
+PASS=$3
 MNTDIR=$(mktemp -d)
 SIZE=100M
 
@@ -16,12 +17,23 @@ rm -i $DISK
 [ ! -d $INPUTDIR ] && echo "Error: input directory not found!" && exit 0
 [ -f $DISK ] && echo "Error: disk file already exists!" && exit 0
 
+
 dd if=/dev/zero of=$DISK bs=$SIZE count=1   # empty floppy disk image
 /sbin/mkfs.vfat $DISK
 
-sudo mount -o loop,uid=$UID,gid=$UID $DISK $MNTDIR
+# chmod 777 $MNTDIR
+# ls -ld $MNTDIR
 # sudo mount -o loop $DISK $MNTDIR
-cp -v -rf $INPUTDIR/* $MNTDIR
+sudo mount -o loop,uid=$UID,gid=$UID $DISK $MNTDIR
+# chmod 777 $MNTDIR
+ls -ld $MNTDIR
+
+export PASS
+( cd $INPUTDIR ; tar cvf $MNTDIR/$DISK.tar * )
+openssl enc -in $MNTDIR/$DISK.tar -out $MNTDIR/$DISK.tar.enc -pass env:PASS
+# rm -f $MNTDIR/$DISK.tar
 sudo umount $MNTDIR
+
+# rm -rf $MNDIR
 
 # eof
